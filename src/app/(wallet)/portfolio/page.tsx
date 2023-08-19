810e4f2e6a3bf1a859a4d81f7bd6ba292ade788b
lucas-dash <homolka.lukas10@gmail.com>
2023-08-19 10:19:41 +0200

    portfolio, watchlist page

---
+++ src/app/(wallet)/portfolio/page.tsx
@@ -0,0 +1,10 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Portfolio | Coin Wallet',
+  description: 'Your coin portfolio.',
+};
+
+export default function Portfolio() {
+  return <section>Portfolio</section>;
+}
